20e75772c8dfb021587aa54b05610cfb7887268b
Anthony Montaigne <fuzznimp@Anthonys-MacBook-Pro.local>
2020-02-21 11:22:52 +0100

    added preact + typescript plugin

---
+++ preact-typescript.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true
+  },
+  extends: [
+    "./index",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 6,
+    sourceType: "module"
+  },
+  rules: {
+    "react/no-unknown-property": ["error", { ignore: ["class"] }]
+  },
+  settings: {
+    react: {
+      pragma: "h",
+      version: "detect"
+    }
+  }
+};
